f2637f55675efa409c7fb1aa256a25279e77a8cd
linkwire <00e5ee@gmail.com>
2022-07-29 18:23:47 -0700

    update frame

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import blind75 as bs
+import numpy as np
 
 if __name__ == '__main__':
-    rand = [3, 9, 2, 4, 7, 2, 12, 6]
-    print(bs.watershed(rand))
-
-    a = [2, 3, -2, 4]
-    b = [-2, 0, -4]
-
-    print(bs.max_product(a))
-    print(bs.max_product(b))
+    print("hello world")
